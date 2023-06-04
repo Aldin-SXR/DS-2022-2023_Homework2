ef4579836f55eefc350210ed851e96b0718ae2b9
Aldin Kovačević <aldin.kovacevic.97@gmail.com>
2023-06-04 10:55:44 +0200

    Update RedBlackTree.java

--- src/main/java/task2/RedBlackTree.java
+++ src/main/java/task2/RedBlackTree.java
@@ -4,7 +4,7 @@ public class RedBlackTree <Key extends Comparable<Key>, Value> {
     // Implement the required properties of a red-black tree
 
     // track the number of iterations needed for the get() operation to complete
-    private int numSteps = 0;
+    public int numSteps = 0;
 
     public Value get(Key key) {
         // implement the actual logic (remove next line)
