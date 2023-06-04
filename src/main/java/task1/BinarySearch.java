af36fccf093c127c3208f29cdcbdc4f435817b6e
Aldin Kovačević <aldin.kovacevic.97@gmail.com>
2023-06-04 10:55:29 +0200

    Update BinarySearch.java

--- src/main/java/task1/BinarySearch.java
+++ src/main/java/task1/BinarySearch.java
@@ -4,7 +4,7 @@ public class BinarySearch {
     // track the number of iterations needed for a search to complete
     public static int numSteps = 0;
 
-    public static int search(Student[] students) {
+    public static int search(Student[] students, int key) {
         // implement the actual logic (remove next line)
         return -1;
     }
